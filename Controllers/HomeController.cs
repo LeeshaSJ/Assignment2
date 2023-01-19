121f770e7106c5edbcb619b9c52cc830155f3eec
LeeshaSJ <leeshajayasekara@gmail.com>
2023-01-20 00:34:33 +0530

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Assignment2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Assignment2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Student()
+        {
+            return View();
+        }
+
+        public IActionResult Staff()
+        {
+            return View();
+        }
+        public IActionResult MyRequests()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ wwwroot/js/student.js
@@ -0,0 +1,22 @@
+﻿$(function () {
+    $("#selector-1").click(function () {
+        $(".applied-project").hide();
+    })
+    $("#books").hide();
+    $("#devices").hide();
+
+
+
+    $("#dropdown-books").click(function () {
+        $("#books").show();
+        $("#devices").hide();
+    });
+
+
+
+    $("#dropdown-devices").click(function () {
+        $("#devices").show();
+        $("#books").hide();
+    });
+})
+
