9ee46a7b3f6ae36535c04e8ee3d6946bf39811ac
LeeshaSJ <leeshajayasekara@gmail.com>
2023-01-20 21:11:58 +0530

    JS part completed in student view and did some decorations blaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- wwwroot/js/student.js
+++ wwwroot/js/student.js
@@ -1,22 +1,53 @@
 ﻿$(function () {
-    $("#selector-1").click(function () {
-        $(".applied-project").hide();
-    })
+    $(".applied-project").hide();
+    $(".advance-web-dev").hide();
+    $(".online-business-sys").hide();
+    $(".ict-bus-analytics").hide();
     $("#books").hide();
     $("#devices").hide();
 
+    $("#selector-1").click(function () {
+        $(".applied-project").show();
+        $(".advance-web-dev").hide();
+        $(".online-business-sys").hide();
+        $(".ict-bus-analytics").hide();
+        $("#dropdown-unit").text("Applied Project");
+    })
 
+    $("#selector-2").click(function () {
+        $(".advance-web-dev").show();
+        $(".applied-project").hide();
+        $(".online-business-sys").hide();
+        $(".ict-bus-analytics").hide();
+        $("#dropdown-unit").text("Advance Web Development");
+    })
 
+    $("#selector-3").click(function () {
+        $(".online-business-sys").show();
+        $(".applied-project").hide();
+        $(".advance-web-dev").hide();
+        $(".ict-bus-analytics").hide();
+        $("#dropdown-unit").text("Online Business System Development");
+        
+    })
+    $("#selector-4").click(function () {
+        $(".ict-bus-analytics").show();
+        $(".applied-project").hide();
+        $(".advance-web-dev").hide();
+        $(".online-business-sys").hide();
+        $("#dropdown-unit").text("ICT Business Analytics and Data Visualization");
+    })
+   
     $("#dropdown-books").click(function () {
         $("#books").show();
         $("#devices").hide();
+        $("#dropdown-category").text("Books");
     });
-
-
 
     $("#dropdown-devices").click(function () {
         $("#devices").show();
         $("#books").hide();
+        $("#dropdown-category").text("Devices");
     });
 })
 
