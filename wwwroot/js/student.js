4a070c222bdec73a33519c374a2c5509cc2a50fc
LeeshaSJ <leeshajayasekara@gmail.com>
2023-01-25 11:51:38 +0530

    Did some decs sdkfjo

--- wwwroot/js/student.js
+++ wwwroot/js/student.js
@@ -1,33 +1,33 @@
 ﻿$(function () {
     $("#selector-1").click(function () {
-        $(".applied-project").show();
         $(".advance-web-dev").hide();
         $(".online-business-sys").hide();
         $(".ict-bus-analytics").hide();
+        $(".applied-project").show();
         $("#dropdown-unit").text("Applied Project");
     })
 
     $("#selector-2").click(function () {
-        $(".advance-web-dev").show();
         $(".applied-project").hide();
         $(".online-business-sys").hide();
         $(".ict-bus-analytics").hide();
+        $(".advance-web-dev").show();
         $("#dropdown-unit").text("Advance Web Development");
     })
 
     $("#selector-3").click(function () {
-        $(".online-business-sys").show();
         $(".applied-project").hide();
         $(".advance-web-dev").hide();
         $(".ict-bus-analytics").hide();
+        $(".online-business-sys").show();
         $("#dropdown-unit").text("Online Business System Development");
         
     })
     $("#selector-4").click(function () {
-        $(".ict-bus-analytics").show();
         $(".applied-project").hide();
         $(".advance-web-dev").hide();
         $(".online-business-sys").hide();
+        $(".ict-bus-analytics").show();
         $("#dropdown-unit").text("ICT Business Analytics and Data Visualization");
     })
    
