c026d19c90edd17664ccf24fffc211697adcade5
PMMalinduRK <120358431+PMMalinduRK@users.noreply.github.com>
2023-02-01 10:10:34 +0530

    DB initial

---
+++ Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Assignment2.Models
+{
+    public class UserModel
+    {
+        [Key]
+        public string Id { get; set; }
+        [Required]
+        public string FirstLastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Department { get; set; }
+    }
+}
